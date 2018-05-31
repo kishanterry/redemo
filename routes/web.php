81492295d1e4f3a6692c2d1a73fcc7b5de3e2f28
Amith Gotamey <kishanterry@gmail.com>
2018-06-01 00:01:44 +0200

    create url bug fixed

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,8 @@
 
 Route::patch('/projects/{project}', 'ProjectsController@update')->name('projects.update')->middleware('auth');
 Route::post('/projects', 'ProjectsController@store')->name('projects.store')->middleware('auth');
-Route::get('/projects/{project}', 'ProjectsController@edit')->name('projects.edit')->middleware('auth');
 Route::get('/projects/create', 'ProjectsController@create')->name('projects.create')->middleware('auth');
+Route::get('/projects/{project}', 'ProjectsController@edit')->name('projects.edit')->middleware('auth');
 Route::get('/projects', 'ProjectsController@index')->name('projects.index')->middleware('auth');
 
 Route::get('/', 'HomeController@index')->name('home');
